501793c571626d09f7b5723f2e0ba1e86c0c3d31
Saikrishna-K <Saikrishna-K@missionrnd.com>
2016-03-18 20:57:22 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,5 +19,64 @@ struct node {
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+
+	if (head1 == NULL && head2 == NULL)  return NULL;
+	struct node *temp1,  *list;
+
+
+	// if any one of the head node is NULL then returning the other node which is not NULL.
+	if (head1 == NULL && head2 != NULL) return head2;
+	if (head1 != NULL && head2 == NULL) return head1;
+
+
+
+	// to retrieve the first node inorder to return  as  initial element after merging.
+	if (head1->num < head2->num)
+	{
+		list = head1; head1 = head1->next;
+	}
+	else
+	{
+		list = head2;
+		head2 = head2->next;
+	}
+
+	temp1 = list;// to hold the initial node .
+
+	while (head1 != NULL && head2 != NULL)
+	{
+		if (head1->num < head2->num)
+		{
+			list->next = head1;
+			list = list->next;
+			head1 = head1->next;
+
+		}
+
+		else
+		{
+			list->next = head2;
+			list = list->next;
+			head2 = head2->next;
+		}
+
+	}
+
+	if (head1) // if all head2 nodes are inserted in the list , then head1 nodes are attached if any.
+	{
+		list->next = head1;
+
+	}
+	else       // if all head1 nodes are inserted in the list , then head2 nodes are attached if any.
+	{
+		list->next = head2;
+	}
+
+
+
+	return temp1; //returning the initial node.
+
+
+
+
 }
